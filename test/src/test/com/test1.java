61fd16bcefca6743d19199e95bd21f2f1cb24714
blowkissfei <50288451+blowkissfei@users.noreply.github.com>
2019-05-06 09:22:25 +0800

    Update test1.java

--- test/src/test/com/test1.java
+++ test/src/test/com/test1.java
@@ -2,11 +2,8 @@ package test.com;
 
 public class test1 {
 	public static void main(String[] args) {
-		System.out.println("ÄãºÃ git ");
-		System.out.println("ÄãºÃ git ");
-		System.out.println("ÄãºÃ git ");
-		System.out.println("ÄãºÃ git ");System.out.println("ÄãºÃ git ");System.out.println("ÄãºÃ git ");
-		System.out.println("ÄãºÃ git ");
+		System.out.println("i am the king in the world!");
+		
 		
 	}
 
