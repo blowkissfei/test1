8a986cb47f54f61c8f804967a21633d27bda4aa2
UID <UID@DESKTOP-DACJOCN>
2019-05-06 10:45:02 +0800

    44

--- test/src/test/com/test1.java
+++ test/src/test/com/test1.java
@@ -2,21 +2,9 @@ package test.com;
 
 public class test1 {
 	public static void main(String[] args) {
-
-		System.out.println("你好 git ");
-		System.out.println("你好 git ");
-		System.out.println("你好 git ");
-		System.out.println("你好 git ");
-		System.out.println("你好 git ");
-		System.out.println("你好 git ");
-		System.out.println("123123123213 ");
-		System.out.println("1231321 ");
-		System.out.println("131313t ");
-		System.out.println("1313131313131 ");
-
-		System.out.println("i am the king in the world!");
-		
-
+		System.out.println("555555555555555");
+		System.out.println("555555555555555");
+		System.out.println("555555555555555");
 	}
 
 }
